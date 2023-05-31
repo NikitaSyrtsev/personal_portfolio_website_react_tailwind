fbf0a30e57fe7e78cc92a63a6d88c89e501f484f
NikitaSyrtsev <nik.syrtsev02@gmail.com>
2023-05-31 19:37:14 +0300

    added

---
+++ src/data.js
@@ -0,0 +1,93 @@
+import { FiGithub, FiMail, FiMapPin } from "react-icons/fi";
+
+import { SiCodewars, SiTelegram } from "react-icons/si";
+
+import SkillImg1 from "./assets/img/skills/html.svg";
+import SkillImg2 from "./assets/img/skills/css.svg";
+import SkillImg3 from "./assets/img/skills/javascript.svg";
+import SkillImg4 from "./assets/img/skills/typescript.svg";
+import SkillImg5 from "./assets/img/skills/react.svg";
+import SkillImg6 from "./assets/img/skills/tailwind.svg";
+import SkillImg7 from "./assets/img/skills/python.svg";
+import SkillImg8 from "./assets/img/skills/git.svg";
+
+export const navigation = [
+    {
+        name: "home",
+        href: "home",
+    },
+    {
+        name: "about",
+        href: "about",
+    },
+    {
+        name: "portfolio",
+        href: "portfolio",
+    },
+
+    {
+        name: "contact",
+        href: "contact",
+    },
+];
+
+export const social = [
+    {
+        icon: <FiMail />,
+        href: "",
+    },
+    {
+        icon: <SiTelegram />,
+        href: "",
+    },
+    {
+        icon: <FiGithub />,
+        href: "",
+    },
+    {
+        icon: <SiCodewars />,
+        href: "",
+    },
+];
+
+export const skills = [
+    {
+        image: SkillImg1,
+    },
+    {
+        image: SkillImg2,
+    },
+    {
+        image: SkillImg3,
+    },
+    {
+        image: SkillImg4,
+    },
+    {
+        image: SkillImg5,
+    },
+    {
+        image: SkillImg6,
+    },
+    {
+        image: SkillImg7,
+    },
+    {
+        image: SkillImg8,
+    },
+];
+
+export const contact = [
+    {
+        icon: <FiMail />,
+        title: "Have a question?",
+        subtitle: "I am here to help you.",
+        description: "Email me at nik.syrtsev02@gmail.com",
+    },
+    {
+        icon: <FiMapPin />,
+        title: "Current Location",
+        subtitle: "Rostov-on-Don, Russia",
+        description: "",
+    },
+];
