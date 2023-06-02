138908e2a0cf5815978b49a8229977fba6c2cb8b
NikitaSyrtsev <nik.syrtsev02@gmail.com>
2023-06-02 17:24:16 +0300

    added

--- src/App.js
+++ src/App.js
@@ -15,8 +15,8 @@ function App() {
         <Header />
         <Hero />
         <About />
-        <Services />
         <Skills />
+        <Services />
         <Projects />
         <Contact />
         <Footer />
--- src/components/Skills.js
+++ src/components/Skills.js
@@ -3,7 +3,7 @@ import { skills } from "../data";
 
 const Skills = () => {
     return (
-        <section className="bg-tertiary py-12">
+        <section className="bg-primary py-12">
             <div className="container mx-auto">
                 <div className="grid grid-cols-8 md:grid-flow-row">
                     {skills.map((skill, index) => {
