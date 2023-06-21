001afedcc9b478a56d0b70a319a85b8108de24ae
NikitaSyrtsev <nik.syrtsev02@gmail.com>
2023-06-21 23:27:55 +0300

    added

--- src/components/NotFoundPage.jsx
+++ src/components/NotFoundPage.jsx
@@ -6,7 +6,7 @@ const NotFoundPage = () => {
         <div className="flex flex-col items-center justify-center h-screen bg-primary">
             <h1 className="text-4xl font-bold mb-4">404 - Page Not Found</h1>
             <p className="text-lg text-accent">Oops! The page you are looking for does not exist.</p>
-            <Link to="/" className="mt-4 px-4 py-2 bg-accent text-white rounded-full hover:bg-accent-hover transition-all duration-300">Back to Home Page</Link>
+            <Link to="/personal_portfolio_website" className="mt-4 px-4 py-2 bg-accent text-white rounded-full hover:bg-accent-hover transition-all duration-300">Back to Home Page</Link>
         </div>
     );
 }
